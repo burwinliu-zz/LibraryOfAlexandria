3c32056a9d1d44e2ffaadecd1a07bfc2f945d2eb
burwinliu <42106619+burwinliu@users.noreply.github.com>
2021-02-10 14:16:45 -0800

    having markers per chest

--- MalmoPlatform/Malmo/samples/Python_examples/library/main.py
+++ MalmoPlatform/Malmo/samples/Python_examples/library/main.py
@@ -45,14 +45,14 @@ def GetMissionXML():
     roof = "<DrawCuboid x1='15' y1='10' z1='-10' x2='-2' y2='10' z2='2' type='bookshelf' />"
     floor = "<DrawCuboid x1='15' y1='1' z1='-10' x2='-2' y2='1' z2='2' type='bookshelf' />"
     torches = ""
-    for i in range(-1,15,2):
+    for i in range(-1, 15, 2):
         torches += f"<DrawBlock x='{i}' y='5' z='1' type='torch' face='NORTH' />"
         torches += f"<DrawBlock x='{i}' y='7' z='1' type='torch' face='NORTH' />"
         torches += f"<DrawBlock x='{i}' y='9' z='1' type='torch' face='NORTH' />"
         torches += f"<DrawBlock x='{i}' y='5' z='-9' type='torch' face='SOUTH' />"
         torches += f"<DrawBlock x='{i}' y='7' z='-9' type='torch' face='SOUTH' />"
         torches += f"<DrawBlock x='{i}' y='9' z='-9' type='torch' face='SOUTH' />"
-    for i in range(-9,2,2):
+    for i in range(-9, 2, 2):
         torches += f"<DrawBlock x='14' y='5' z='{i}' type='torch' face='WEST' />"
         torches += f"<DrawBlock x='14' y='7' z='{i}' type='torch' face='WEST' />"
         torches += f"<DrawBlock x='14' y='9' z='{i}' type='torch' face='WEST' />"
@@ -79,7 +79,9 @@ def GetMissionXML():
                             <ServerHandlers>
                                 <FlatWorldGenerator generatorString="3;7,2;1;"/>
                                 <DrawingDecorator>
-                                    <DrawBlock x='0' y='2' z='1' type='ender_chest' />{libraryEnv}
+                                    {libraryEnv}
+                                    <DrawBlock x='0' y='2' z='1' type='ender_chest' />
+                                    <DrawBlock x='0' y='1' z='0' type='emerald_block' />
                                 </DrawingDecorator>
                                 <ServerQuitWhenAnyAgentFinishes/>
                                     
@@ -110,57 +112,62 @@ def GetMissionXML():
                         </AgentSection>
                     </Mission>'''
 
-def end(agent_host, world_state):
-    print("Ending Mission", end = ' ')
-    while world_state.is_mission_running:
+
+def end(arg_agent_host, arg_world_state):
+    print("Ending Mission", end=' ')
+    while arg_world_state.is_mission_running:
         print(".", end="")
-        agent_host.sendCommand("moveeast")
+        arg_agent_host.sendCommand("moveeast")
         time.sleep(0.1)
-        world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
+        arg_world_state = arg_agent_host.getWorldState()
+        for end_error in world_state.errors:
+            print("Error:", end_error.text)
 
-def moveLeft(agent_host, steps):
+
+def moveLeft(arg_agent_host, steps):
     for i in range(steps):
-        agent_host.sendCommand("moveeast")
+        arg_agent_host.sendCommand("moveeast")
         time.sleep(0.1)
 
-def moveRight(agent_host, steps):
+
+def moveRight(arg_agent_host, steps):
     for i in range(steps):
-        agent_host.sendCommand("movewest")
+        arg_agent_host.sendCommand("movewest")
         time.sleep(0.1)
 
-def moveToChest(agent_host, chestNum):
+
+def moveToChest(arg_agent_host, chest_num):
     global agent_position
-    if agent_position == chestNum:
+    if agent_position == chest_num:
         return
-    print(f"Moving to chest #{chestNum} ..", end=' ')
-    if chestNum == 7: # if moving to ender chest
-        while agent_position < 6: # move to chest 6
-            moveRight(agent_host, 2)
+    print(f"Moving to chest #{chest_num} ..", end=' ')
+    if chest_num == 7:  # if moving to ender chest
+        while agent_position < 6:  # move to chest 6
+            moveRight(arg_agent_host, 2)
             agent_position += 1
-        moveRight(agent_host, 3) # move to ender chest
+        moveRight(arg_agent_host, 3)  # move to ender chest
         agent_position = 7
         print("done")
         return
-    if agent_position == 7: # if starting at ender chest
-        moveLeft(agent_host, 3) # move to chest 6
+    if agent_position == 7:  # if starting at ender chest
+        moveLeft(arg_agent_host, 3)  # move to chest 6
         agent_position = 6
-        while chestNum < agent_position: # move to chest
-            moveLeft(agent_host, 2)
+        while chest_num < agent_position:  # move to chest
+            moveLeft(arg_agent_host, 2)
             agent_position -= 1
         print("done")
         return
     # moving chest -> chest
-    if agent_position > chestNum: # if chest is on the left
-        while agent_position > chestNum:
-            moveLeft(agent_host, 2)
+    if agent_position > chest_num:  # if chest is on the left
+        while agent_position > chest_num:
+            moveLeft(arg_agent_host, 2)
             agent_position -= 1
-    elif agent_position < chestNum: # if chest is on the right
-        while agent_position < chestNum:
-            moveRight(agent_host, 2)
+    elif agent_position < chest_num:  # if chest is on the right
+        while agent_position < chest_num:
+            moveRight(arg_agent_host, 2)
             agent_position += 1
     print("done")
+
 
 if __name__ == '__main__':
     # Create default Malmo objects:
@@ -183,7 +190,7 @@ if __name__ == '__main__':
         time.sleep(0.1)
         world_state = agent_host.getWorldState()
         for error in world_state.errors:
-            print("Error:",error.text)
+            print("Error:", error.text)
 
     print()
     print("Mission running..")
@@ -199,12 +206,14 @@ if __name__ == '__main__':
             if i not in chests[nChest]:
                 chests[nChest][i] = 0
             chests[nChest][i] += 1
-    for i in range(size):
+    for chest_num in range(size):
         # /setblock ~3 ~2 ~1 minecraft:chest 2 replace {Items:[{Slot:0,id:stone,Count:1b}]}
 
         itemString = ",".join([f"{{Slot:{num}, id:{value[0]},Count:{value[1]}b}}"
-                               for num, value in enumerate(chests[i].items())])
-        agent_host.sendCommand(f"chat /setblock {i * 2 + 3} 2 1 minecraft:chest 2 replace {{Items:[{itemString}]}}")
+                               for num, value in enumerate(chests[chest_num].items())])
+        agent_host.sendCommand(f"chat /setblock {chest_num * 2 + 3} 1 0 minecraft:diamond_block 2 replace")
+        agent_host.sendCommand(f"chat /setblock {chest_num * 2 + 3} 2 1 "
+                               f"minecraft:chest 2 replace {{Items:[{itemString}]}}")
     print("done")
 
     time.sleep(2)
