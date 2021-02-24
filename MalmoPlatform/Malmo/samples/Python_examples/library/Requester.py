57ae1d28ce1473434999abf5f7f7b9d14eab3f15
burwinliu <42106619+burwinliu@users.noreply.github.com>
2021-02-24 15:14:53 -0800

    updating init params

--- MalmoPlatform/Malmo/samples/Python_examples/library/Requester.py
+++ MalmoPlatform/Malmo/samples/Python_examples/library/Requester.py
@@ -1,6 +1,8 @@
 class Requester:
-    def __init__(self, max_input):
+    def __init__(self, max_input, stochastic, available_input):
         self.max_input = max_input
+        self.stochastic = stochastic
+        self.available = available_input
 
     def get_request(self):
         return {}
